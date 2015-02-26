ccf36ba880c89cb061f95e52bea15b030ef0e58c
Jose David Baena <josedab@gmail.com>
2015-02-26 12:15:04 +0000

    Finished ted oembed client - Used rest client to do requests to the tedtalks oembed endpoint

--- src/main/java/com/technobitia/ted/client/TedOembedClient.java
+++ src/main/java/com/technobitia/ted/client/TedOembedClient.java
@@ -2,15 +2,36 @@ package com.technobitia.ted.client;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import com.technobitia.ted.example.VideoMediaInformationApp;
 import com.technobitia.ted.model.TedResource;
 
+/**
+ * Client for requesting embedded representation of a url on TED website. Works
+ * with the oembed type video. For more information about its use, @see
+ * {@link VideoMediaInformationApp}
+ */
 public class TedOembedClient {
+
+    private static final String TED_TALKS_BASE_URL = "http://www.ted.com/talks";
+    private static final String OEMBED_PATH = "oembed.json";
+
+    private Client restClient;
+    private WebTarget baseTarget;
+
+    public TedOembedClient() {
+        restClient = ClientBuilder.newClient();
+        baseTarget = restClient.target(TED_TALKS_BASE_URL).path(OEMBED_PATH);
+    }
 
     public TedResource extractOEmbedInformationFromUrl(String url) {
         checkNotNull(url);
+        WebTarget webTarget = baseTarget.queryParam("url", url);
 
-        TedResource tedResource = null;
-
+        TedResource tedResource = webTarget.request().get(TedResource.class);
         return tedResource;
     }
 }
